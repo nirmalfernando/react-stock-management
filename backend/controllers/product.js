17d34fad8865142cf50800de7a08c57af80ab262
Nirmal Fernando <112607619+nirmalfernando@users.noreply.github.com>
2024-04-03 22:25:28 +0530

    Add files via upload

---
+++ backend/controllers/product.js
@@ -0,0 +1,19 @@
+export const getPosts = (req, res) => {
+  res.json("from controller");
+};
+
+export const getPost = (req, res) => {
+  res.json("from controller");
+};
+
+export const addPost = (req, res) => {
+  res.json("from controller");
+};
+
+export const deletePost = (req, res) => {
+  res.json("from controller");
+};
+
+export const updatePost = (req, res) => {
+  res.json("from controller");
+};

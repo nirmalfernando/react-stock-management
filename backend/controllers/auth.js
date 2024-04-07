52c8c005f6df522cdca11536d002b54600df5609
Nirmal Fernando <112607619+nirmalfernando@users.noreply.github.com>
2024-04-07 13:29:57 +0530

    Update auth.js

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,41 +1,78 @@
-import { db } from "../db.js";
+import { db } from "../connect.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
-  //Check existing user
-  const q = "SELECT * from user WHERE UserID = ?";
+  //CHECK IF THE USER ALREADY EXISTS
 
-  db.query(q, [req.body.UserID], (err, data) => {
-    if (err) return res.json(err);
+  const q = "SELECT * FROM user WHERE userid = ?";
+
+  db.query(q, [req.body.userid], (err, data) => {
+    if (err) return res.status(500).json(err);
     if (data.length) return res.status(409).json("User already exists!");
 
-    //Hash the password and create a User
+    //CREATE A NEW USER
+    //ENCRYPTING THE PASSWORD
     const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(req.body.password, salt);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
 
-    //Register a new user to the Database
     const q =
-      "INSERT INTO user (`UserID`, `UserName`, `Name`, `Email`, `PhoneNo`, `Password `,`Image`,`Role`, `Status`,`StatusDate`) VALUES (?)";
+      "INSERT INTO user (`userid`,`username`,`name`,`password`,`email`,`phoneno`,`image`,`role`) VALUES (?)";
+
     const values = [
-      req.body.UserID,
-      req.body.UserName,
-      req.body.Name,
-      req.body.Email,
-      req.body.PhoneNo,
-      hash,
-      req.body.Image,
-      req.body.Role,
-      req.body.Status,
-      req.body.StatusDate,
+      req.body.userid,
+      req.body.username,
+      req.body.name,
+      hashedPassword,
+      req.body.email,
+      req.body.phoneno,
+      req.body.image,
+      req.body.role,
     ];
 
     db.query(q, [values], (err, data) => {
       if (err) return res.json(err);
-      return res.status(200).json("User has been successfully created!");
+      return res.status(200).json("User has been created!");
     });
   });
 };
 
-export const login = (req, res) => {};
+export const login = (req, res) => {
+  //CHECK IF THE USER ALREADY EXISTS
 
-export const logout = (req, res) => {};
+  const q = "SELECT * FROM user WHERE username = ?";
+
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.length === 0) return res.status(404).json("User not found!");
+
+    const checkPassword = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+
+    if (!checkPassword)
+      return res.status(400).json("Wrong Password or Username!");
+
+    const token = jwt.sign({ id: data[0].userid }, process.env.JWT);
+
+    const { password, ...others } = data[0];
+
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(others);
+  });
+};
+
+export const logout = (req, res) => {
+  res
+    .clearCookie("accessToken", {
+      secure: true,
+      sameSite: "none",
+    })
+    .status(200)
+    .json("User has been logged out!");
+};

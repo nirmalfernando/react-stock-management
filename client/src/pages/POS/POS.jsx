98fdc0354a0945ae0b295d8b445904a78eb3a739
Nirmal Fernando <112607619+nirmalfernando@users.noreply.github.com>
2024-04-16 20:45:48 +0530

    Update POS.jsx

--- client/src/pages/POS/POS.jsx
+++ client/src/pages/POS/POS.jsx
@@ -1,42 +1,33 @@
-import React from 'react';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { AiOutlineShoppingCart } from 'react-icons/ai';
-import Card from './Card/Card'; 
-import './POS.css'; 
-import { useNavigate } from 'react-router-dom';
-
+import React, { useEffect, useState } from "react";
+import { AiOutlineSearch, AiOutlineShoppingCart } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+import "./POS.css";
+import axios from "axios";
 
 const ProductPage = () => {
+  const [products, setProducts] = useState([]);
+
+  const [err, setErr] = useState(null);
+
+  useEffect(() => {
+    const fetchAllProducts = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/api/products/product")
+        setProducts(res.data)
+      } catch (err) {
+        setErr(err.response.data);
+      }
+    };
+    fetchAllProducts()
+  },[]);
 
   const navigate = useNavigate();
 
   const handleCart = (e) => {
-    navigate("/bill")
+    navigate("/bill");
     e.preventDefault();
-    
-    console.log('Login clicked');
+    console.log("Cart clicked");
   };
-  
-  const products = [
-    {
-      id: 1,
-      name: 'Product 1',
-      price: 10,
-      imageUrl: 'https://via.placeholder.com/300x200', 
-    },
-    {
-      id: 2,
-      name: 'Product 2',
-      price: 20,
-      imageUrl: 'https://via.placeholder.com/300x200',
-    },
-    {
-      id: 3,
-      name: 'Product 3',
-      price: 20,
-      imageUrl: 'https://via.placeholder.com/300x200', 
-    },
-  ];
 
   return (
     <div className="product-page">
@@ -49,11 +40,27 @@ const ProductPage = () => {
           <option value="price">Price</option>
           <option value="name">Name</option>
         </select>
-        <button className="cart-button"onClick={handleCart}><AiOutlineShoppingCart /></button>
+        <button className="cart-button" onClick={handleCart}>
+          <AiOutlineShoppingCart />
+        </button>
       </div>
       <div className="product-list">
         {products.map((product) => (
-          <Card key={product.id} product={product} />
+          <div key={product.id} className="card">
+            <div className="card-header">{product.productname}</div>
+            <div className="card-subtitle">{product.productname}</div>
+            <img
+              src={product.image}
+              alt={product.productname}
+              className="card-img"
+            />
+            <div className="card-details">
+              <button className="add-to-cart-button">
+                <span className="price">${product.sellingprice}</span>
+                <span className="add-to-cart-icon">+</span>
+              </button>
+            </div>
+          </div>
         ))}
       </div>
     </div>

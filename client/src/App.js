6d3f664315f615a0f7cd6d3b955990be12ee47dd
Nirmal Fernando <112607619+nirmalfernando@users.noreply.github.com>
2024-04-16 12:18:52 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import User from "./pages/User";
@@ -6,6 +5,14 @@ import Add from "./pages/Add";
 import Update from "./pages/Update";
 import RegistrationForm from "./pages/RegisterPage/RegistrationForm";
 import Login from "./pages/LoginPage/Login";
+import SideBar from "./SideBar";
+import Product from "./pages/Products/Products";
+import Dashboard from "./pages/Dashboard/DashContent";
+import POS from "./pages/POS/POS";
+import Inventory from "./pages/Inventory/Inventory";
+import BillInfo from "./BillInfo/BillInfo";
+import GoodReceive from "./pages/GoodReceive/GoodReceive";
+import GoodReturn from "./pages/GoodReturn/GoodReturn";
 import { useContext } from "react";
 import { AuthContext } from "./context/authContext";
 
@@ -17,7 +24,16 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/register" element={<RegistrationForm />} />
+          <Route path="/side" element={<SideBar />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/pos" element={<POS />} />
+          <Route path="/inventory" element={<Inventory />} />
+          <Route path="/bill" element={<BillInfo />} />
+          <Route path="/reservation" element={<GoodReceive />} />
+          <Route path="/return" element={<GoodReturn />} />
           <Route path="/user" element={<User />} />
         </Routes>
       </BrowserRouter>

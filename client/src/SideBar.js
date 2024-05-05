6cb04343c5cfae5f302934d2c5ee5c2641e0b2ac
Pasith Senevirathna <pasiya20024@gmail.com>
2024-05-06 00:49:13 +0530

    Updated SideBar.js (Admin User Controller)

--- client/src/SideBar.js
+++ client/src/SideBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./SideBar.css";
 import Products from "./pages/Products/Products";
 import { FaDisplay, FaRegUser } from "react-icons/fa6";
@@ -12,15 +12,31 @@ import POS from "./pages/POS/POS";
 import Inventory from "./pages/Inventory/Inventory";
 import { useNavigate } from "react-router-dom";
 import Category from "./pages/Category/Category";
-import { handleMobileClick } from "./pages/Category/Category";
 
 function Dashboard() {
   const [activeTab, setActiveTab] = useState("dashboard");
   const navigate = useNavigate();
+  const [role, setRole] = useState(null); // Initialize role state
 
-  const handleImageClick = () => {
-    setActiveTab("inventory"); // Set active tab to 'inventory'
-  };
+  useEffect(() => {
+    let user = JSON.parse(localStorage.getItem("user")); // Get role from localStorage
+    let role = user?.role; // Access role property with optional chaining
+    setRole(role); // Set role state with retrieved value
+  }, []); // Empty dependency array ensures this effect runs only once after initial render
+
+  const tabs = [
+    { label: "Dashboard", icon: FaDisplay, roles: ["Admin", "User"] },
+    { label: "Inventory", icon: FaApple, roles: ["Admin", "User"] },
+    {
+      label: "Product",
+      icon: HiMiniDevicePhoneMobile,
+      roles: ["Admin", "User"],
+    },
+    { label: "POS", icon: BsSuitcaseLg, roles: ["Admin"] },
+    { label: "Customer", icon: FaRegUser, roles: ["Admin"] },
+    { label: "Reports", icon: BiMessageRounded, roles: ["Admin"] },
+  ];
+
   const changeActiveTab = (tab) => {
     setActiveTab(tab);
   };
@@ -30,50 +46,28 @@ function Dashboard() {
       <div className="sidebar">
         <div className="shopname">
           <BsPhoneFlip />
-          <h2 style={{ marginBottom: "20px", marginTop: "10px" }}>I-PLUS</h2>
+          <h2 style={{ marginBottom: "20px", marginTop: "10px" }}>
+            I-PLUS
+          </h2>
         </div>
-        <button
-          className={activeTab === "dashboard" ? "active" : ""}
-          onClick={() => changeActiveTab("dashboard")}
-        >
-          <FaDisplay />
-          <span>Dashboard</span>
-        </button>
-        <button
-          className={activeTab === "inventory" ? "active" : ""}
-          onClick={() => changeActiveTab("inventory")}
-        >
-          <FaApple />
-          <span>Inventory</span>
-        </button>
-        <button
-          className={activeTab === "product" ? "active" : ""}
-          onClick={() => changeActiveTab("product")}
-        >
-          <HiMiniDevicePhoneMobile />
-          <span>Product</span>
-        </button>
-        <button
-          className={activeTab === "pos" ? "active" : ""}
-          onClick={() => changeActiveTab("pos")}
-        >
-          <BsSuitcaseLg />
-          <span>POS</span>
-        </button>
-        <button
-          className={activeTab === "customer" ? "active" : ""}
-          onClick={() => changeActiveTab("customer")}
-        >
-          <FaRegUser />
-          <span>Customer</span>
-        </button>
-        <button
-          className={activeTab === "reports" ? "active" : ""}
-          onClick={() => changeActiveTab("reports")}
-        >
-          <BiMessageRounded />
-          <span>Reports</span>
-        </button>
+        {role && // Render tabs only if role is defined
+          tabs.map((tab, index) => {
+            if (tab.roles.includes(role)) {
+              return (
+                <button
+                  key={index}
+                  className={
+                    activeTab === tab.label.toLowerCase() ? "active" : ""
+                  }
+                  onClick={() => changeActiveTab(tab.label.toLowerCase())}
+                >
+                  <tab.icon />
+                  <span>{tab.label}</span>
+                </button>
+              );
+            }
+            return null;
+          })}
       </div>
 
       <div className="content">
@@ -86,7 +80,7 @@ function Dashboard() {
       </div>
     </div>
   );
-}
+};
 
 function DashboardTab() {
   return (
